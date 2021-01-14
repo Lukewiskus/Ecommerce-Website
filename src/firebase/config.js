c98b0c0f900b88d32a0b68e5c100637c5eb895af
Luke Wiskus <63059998+Lukewiskus@users.noreply.github.com>
2021-01-14 16:13:39 -0600

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,2 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDdwCxawmK6mVDjbKGq-ol5j0PWPcvivts",
-    authDomain: "mercil-leather-website.firebaseapp.com",
-    projectId: "mercil-leather-website",
-    storageBucket: "mercil-leather-website.appspot.com",
-    messagingSenderId: "558140427927",
-    appId: "1:558140427927:web:d0eae1238681f22e7e63d2",
-    measurementId: "G-HQ2D0JP4EE"
 };
